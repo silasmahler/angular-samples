02239906178452a055793fad41d39cc63772e28c
SilasMahler <simah002@stud.fh-dortmund.de>
2017-10-13 09:40:31 +0200

    added first routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component'
 
-
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'books',
+        component: BookListComponent
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/books'
+      }
+];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
