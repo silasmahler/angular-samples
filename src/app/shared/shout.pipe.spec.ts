2e69020c15c86fea591076f57d2117d832d32fd8
SilasMahler <simah002@stud.fh-dortmund.de>
2017-10-14 13:48:45 +0200

    added a pipe test

--- src/app/shared/shout.pipe.spec.ts
+++ src/app/shared/shout.pipe.spec.ts
@@ -5,4 +5,9 @@ describe('ShoutPipe', () => {
     const pipe = new ShoutPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return: blabla', () => {
+    const pipe = new ShoutPipe();
+    // expect(pipe.transform(1000, 'Anzahl')).toBe('Anzahl: 1000');
+  });
 });
