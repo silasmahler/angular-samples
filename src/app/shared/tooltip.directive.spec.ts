1b47abac77844c2ef39f825d8d2d59dc86432381
SilasMahler <simah002@stud.fh-dortmund.de>
2017-10-14 13:40:50 +0200

    added directivetest

--- src/app/shared/tooltip.directive.spec.ts
+++ src/app/shared/tooltip.directive.spec.ts
@@ -1,8 +1,41 @@
+import { By } from '@angular/platform-browser';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement, Directive } from '@angular/core';
 import { TooltipDirective } from './tooltip.directive';
 
-describe('TooltipDirective', () => {
-  it('should create an instance', () => {
-    /*const directive = new TooltipDirective();
-    expect(directive).toBeTruthy();*/
+
+@Component({
+  selector: 'mock-component',
+  template: '<div tooltip="Hallo!"></div>'
+})
+class MockComponent {}
+
+describe('A Component with Tooltipdirective', () => {
+let fixture: ComponentFixture<MockComponent>;
+let element: DebugElement;
+let inst: TooltipDirective;
+
+beforeEach(() => {
+  TestBed.configureTestingModule({
+    declarations: [MockComponent, TooltipDirective]
   });
+
+  fixture = TestBed.createComponent(MockComponent);
+  element = fixture.debugElement.query(By.directive(TooltipDirective));
+  inst = element.injector.get(TooltipDirective);
+  });
+
+it('should create', () => {
+  const tooltipdirective = new TooltipDirective(undefined);
+  expect(tooltipdirective).toBeTruthy();
+});
+
+it('trigger directive when mouseover', () => {
+  fixture.detectChanges();
+  expect(inst.tooltipElement).toBeDefined();
+  expect(inst.tooltipElement).toBeTruthy();
+  element.triggerEventHandler('mouseenter', element);
+  expect(inst.tooltipElement.hidden).toBeFalsy();
+  }
+  );
 });
