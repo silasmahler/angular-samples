48bec19adb2ab7488f4559d255ed4f01a3b8fe93
SilasMahler <simah002@stud.fh-dortmund.de>
2017-10-14 10:05:14 +0200

    direktive angebunden!!!

--- src/app/shared/tooltip.directive.ts
+++ src/app/shared/tooltip.directive.ts
@@ -1,17 +1,25 @@
-import { Directive, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[tooltip]'
 })
 export class TooltipDirective {
-@Input() tooltip: string;
 
-constructor() { }
+  @Input()
+  set tooltip(newVal) {this.tooltipElement.innerText = newVal; }
+
+  tooltipElement: HTMLDivElement = document.createElement('div');
+
+  constructor(private elementRef: ElementRef) {
+    this.elementRef.nativeElement.appendChild(this.tooltipElement);
+  }
 
 @HostListener('mouseenter') onMouseEnter() {
   console.log('Mouse entered Element');
+  this.tooltipElement.hidden = false;
 }
 @HostListener('mouseleave') onMouseLeave() {
   console.log('Mouse left Element');
+  this.tooltipElement.hidden = true;
 }
 }
