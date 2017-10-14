35e8615dd51cf478f789fb64d1be2eca57a0c56b
SilasMahler <simah002@stud.fh-dortmund.de>
2017-10-14 13:48:58 +0200

    added begin of Servicetest

--- src/app/book/shared/book-data.service.spec.ts
+++ src/app/book/shared/book-data.service.spec.ts
@@ -1,3 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { BaseRequestOptions, HttpModule } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { BookDataService } from './book-data.service';
@@ -5,7 +9,18 @@ import { BookDataService } from './book-data.service';
 describe('BookDataService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BookDataService]
+      providers: [
+      BookDataService,
+      MockBackend,
+      BaseRequestOptions, {
+        provide: HttpClient,
+        useFactory: (backendInstance: MockBackend, defaultOptions: BaseRequestOptions) => {
+        return new HttpClient(defaultOptions);
+        }
+      },
+      deps: [MockBackend, BaseRequestOptions]
+    ],
+    imports: [HttpClientTestingModule]
     });
   });
 
