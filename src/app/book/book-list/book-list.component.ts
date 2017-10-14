daa38db6536c4d96c6972a52dcc0f8a5a2cee0de
SilasMahler <simah002@stud.fh-dortmund.de>
2017-10-14 09:23:03 +0200

    added view encapsulation

--- src/app/book/book-list/book-list.component.ts
+++ src/app/book/book-list/book-list.component.ts
@@ -1,27 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {init} from 'protractor/built/launcher';
-import {BookDataService} from "../shared/book-data.service";
-import {Book} from "../shared/book";
+import {BookDataService} from '../shared/book-data.service';
+import {Book} from '../shared/book';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'book-list',
+  encapsulation: ViewEncapsulation.Emulated,
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
   booklist: Observable<Book[]>;
-  
-  
     constructor(private bookService: BookDataService) {
-  
     }
-  
     ngOnInit() {
-      this.booklist = this.bookService.getBooks()
-      console.log(this.booklist)
-    }
-
-
-
+      this.booklist = this.bookService.getBooks();
+      console.log(this.booklist);
   }
+}
